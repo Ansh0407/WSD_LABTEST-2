ce47994047d0b45ac5ac7b4c28c81341caa2c7f5
Ansh Bhandari <97969287+Ansh0407@users.noreply.github.com>
2023-09-19 16:12:36 +0530

    Final Json

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ fetchDogButton.addEventListener('click', () => {
     .catch(error => console.error(error));
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const fetchDogsButton = document.getElementById("fetchDogs");
+    const dogList = document.getElementById("dogList");
+
+    fetchDogsButton.addEventListener("click", function () {
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "dogs.json", true);
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                try {
+                    const dogs = JSON.parse(xhr.responseText);
+                    displayDogs(dogs);
+                } catch (error) {
+                    console.error("Error parsing JSON: " + error.message);
+                }
+            } else {
+                console.error("Error loading dogs: " + xhr.status);
+            }
+        };
+
+        xhr.send();
+    });
+
+    function displayDogs(dogs) {
+        let html = "";
+        for (const dog of dogs) {
+            html += `
+                <div class="card">
+                    <h2>${dog.Breed}</h2>
+                    <p><strong>Color:</strong> ${dog.Color}</p>
+                    <p><strong>Country:</strong> ${dog.Country}</p>
+                </div>
+            `;
+        }
+        dogList.innerHTML = html;
+    }
+});
